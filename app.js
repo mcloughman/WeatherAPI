ef9fc66c8bd8ca6f9ad4ad7b67c95f78606c344a
mcloughman <51391940+mcloughman@users.noreply.github.com>
2020-11-01 21:37:41 -0800

    add .env for API key & write get route and get request to weather api

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 
+// don't need to install https it comes with node just require it
+const https = require("https");
+
 const app = express();
+
+
+// user sends get request to our server
+app.get("/", (req, res) => { 
+ // now we have to make a get request to third party server
+ // create a variable for very long url
+ const url =
+   "https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=API_KEY&units=imperial";
+ https.get(
+  // going to call third party server response instead or res so not to confuse it with our servers response to client
+   url, (response) => {
+     console.log(response);
+   }
+ );
+ res.send("WE are up and running")
+})
 
 app.listen(3000, () => {
  console.log("Server listening on 3000");
