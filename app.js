977497807cf05b82626817b059d5029de1b15a7c
mcloughman <51391940+mcloughman@users.noreply.github.com>
2020-11-02 12:30:29 -0800

    add form and body-parser to make query dynamic to specific user city search

--- app.js
+++ app.js
@@ -3,37 +3,45 @@ const express = require("express");
 
 // don't need to install https it comes with node just require it
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // user sends get request to our server
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+
   // now we have to make a get request to third party server
   // create a variable for very long url
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=${process.env.API_KEY}&units=imperial`;
+
+  const unit = "imperial";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.API_KEY}&units=${unit}`;
   https.get(
     // going to call third party server response instead or res so not to confuse it with our servers response to client
     url,
     (response) => {
-      console.log(response.statusCode);
-
       response.on("data", (data) => {
         const weatherData = JSON.parse(data);
         const temp = Math.round(weatherData.main.temp);
         console.log(temp);
         const weatherDescription = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon;
-        const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        const iconUrl = `http://openweathermap.org/img/wn/${icon}@4x.png`;
         res.send(
-          `<h1>The temperature in Las Vegas is ${temp}.</h1> And the condition is ${weatherDescription}.
-          <br>
-          <img src="${iconUrl}" />`
+          `<h1 style="color: orange;">The temperature in ${query} is ${temp} degrees Fahrenheit.</h1> <h3>And the condition is: ${weatherDescription}</h3>.
+            <br>
+            <img src="${iconUrl}" />`
         );
       });
     }
   );
 });
-
 app.listen(3000, () => {
   console.log("Server listening on 3000");
 });
